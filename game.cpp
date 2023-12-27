2297e0112f01027e96d5728669a6677060d2dee2
Opi22 <khideromer20031610@gmail.com>
2023-12-27 23:05:48 +0200

    not working

--- game.cpp
+++ game.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 void playerX(int, char(&board)[3][3]);
 void playerO(int , char (&board)[3][3] );
+bool win(char (&board)[3][3]);
 void displayBoard(char(& board)[][3]){
  
 for (int i=0; i<3; i++){
@@ -18,17 +19,36 @@ for (int i=0; i<3; i++){
 }
 int main(){
     char board[3][3]={
-        {'1', '2', '3'},
-        {'4', '5', '6'},
-        {'7', '8', '9'}
+        {'X', '2', '3'},
+        {'4', 'X', '6'},
+        {'7', '8', 'X'}
     };
     int count=0;
-    do{
+    bool winner=false;
+    int num;
      displayBoard(board);
-     playerX(9, board);
-     displayBoard(board);
-    count++;
-    }while(count<9);
+     while(winner=false){
+     cout<<"\n Enter A number to palce X:  ";
+     cin>>num;
+    playerX(num, board);
+    if (win(board)){
+        cout <<"X won the game \n";
+        winner =true;
+    } else{
+        displayBoard(board);
+        cout<<"Enter a number to place O:  ";
+        cin>>num;
+        playerO(num, board);
+        if (win(board)){
+            cout<<"O won the game \n";
+            winner = true;
+        } else {
+            winner =false;
+        }
+    }
+    }
+    
+    
 return 0;
 }
 
@@ -64,5 +84,62 @@ void playerX(int num, char (&board)[3][3] ){
         default:
         cout<<"invalid input \n";
         break;
+    }
+}
+
+void playerO(int num, char (&board)[3][3] ){
+    switch(num){
+        case 1:
+        board[0][0]='O';
+        break;
+        case 2:
+        board[0][1]='O';
+        break;
+        case 3:
+        board[0][2]='O';
+        break;
+        case 4:
+        board[1][0]='O';
+        break;
+        case 5:
+        board[1][1]='O';
+        break;
+        case 6:
+        board[1][2]='O';
+         break;
+         case 7:
+         board[2][0]='O';
+         break;
+         case 8:
+         board[2][1]='O';
+         break;
+         case 9:
+         board[2][2]='O';
+         break;
+        default:
+        cout<<"invalid input \n";
+        break;
+    }
+}
+//board[][]==board[][]&&board[][]==board[][]
+bool win(char (&board)[3][3]){
+    if(board[0][0]==board[0][1]&&board[0][1]==board[0][2]){
+        return true;
+    } else if(board[1][0]==board[1][1]&&board[1][1]==board[1][2]){
+        return true;
+    } else if(board[2][0]==board[2][1]&&board[2][1]==board[2][2]){
+        return true;
+    } else if(board[0][0]==board[1][0]&&board[1][0]==board[2][0]){
+        return true;
+    } else if (board[0][1]==board[1][1]&&board[1][1]==board[2][1]){
+        return true;
+    }else if(board[0][2]==board[1][2]&&board[1][2]==board[2][2]){
+        return true;
+    } else if(board[0][0]==board[1][1]&&board[1][1]==board[2][2]){
+       return true;
+    } else if(board[0][2]==board[1][1]&&board[1][1]==board[2][0]){
+     return true;
+    } else {
+        return false;
     }
 }
